42456fe9dc108eaf0b12d43a48b61e26edac825e
Allison  Craig <allison@craig.net>
2015-09-15 11:25:43 -0700

    edit comments

--- static/script.js
+++ static/script.js
@@ -1,49 +1,51 @@
-//Instatiate counter 
+//Instatiate counter for current image
 var current = 0;
 
 //Get images passed through hidden inputs so control with JS
 var imagesDeck = document.getElementsByClassName("image-data");
 
 //Count deck of images so I can use this to make rotation seamless
-var imageDeckLength = imagesDeck.length -1; //because arrays are cardinal and indexes are not, I take the last number off the count to account for index 0
+//because arrays are cardinal and indexes are not, I take the last number off the count to account for index 0
+var imageDeckLength = imagesDeck.length -1; 
 
 //Select Arrows for event handlers
-var target = document.getElementsByClassName("arrow");
+var arrowImgs = document.getElementsByClassName("arrow");
 
 //Select place for image src
-var box = document.getElementsByClassName("display-box");
+var imgDisplay = document.getElementsByClassName("display-box");
 
 //Select place for caption
 var caption = document.getElementsByClassName("caption-box");
 
-//Document events listener for DOM loaded for starting image
+//Document event listener for DOM loaded for starting image
 document.addEventListener("DOMContentLoaded", function() { 
-    box[0].src=imagesDeck[current].name
+    imgDisplay[0].src=imagesDeck[current].name
     caption[0].innerHTML=imagesDeck[current].value
 });
 
 
-target[0].addEventListener("click", function(e) {
+
+// Click Event Handlers for 'Previous' and 'Next' arrows:
+
+// PREV ARROW
+arrowImgs[0].addEventListener("click", function(e) {
     current -= 1
-    // console.log("SUBBED CURRENT", current)
     if (current === -1){
-        current = (imageDeckLength) 
-        // console.log("CHANGED CURRENT FOR -1", current)
+        current = (imageDeckLength) //If i've gone through entire deck with PREV arrow, start at end
     };
     // Change image src and caption text
-    box[0].src=imagesDeck[current].name
+    imgDisplay[0].src=imagesDeck[current].name
     caption[0].innerHTML=imagesDeck[current].value
 });
 
-target[1].addEventListener("click", function(e) {
+// NEXT ARROW
+arrowImgs[1].addEventListener("click", function(e) {
     current += 1
-    // console.log("ADDED CURRENT", current)
     if (current > imageDeckLength){
-        current = 0 //If i've gone through entire deck, start at begining
-        // console.log("CHANGED CURRENT FOR len", current)
+        current = 0 //If i've gone through entire deck with NEXT arrow, start at begining
     }
     // Change image src and caption text
-    box[0].src=imagesDeck[current].name
+    imgDisplay[0].src=imagesDeck[current].name
     caption[0].innerHTML=imagesDeck[current].value
 });
 
