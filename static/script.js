b8dab25b41c760618bd5398abf7797b15ae8583b
Allison  Craig <allison@craig.net>
2015-09-15 09:52:44 -0700

    add comments for debug

--- static/script.js
+++ static/script.js
@@ -24,22 +24,26 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 target[0].addEventListener("click", function(e) {
+    current -= 1
+    console.log("SUBBED CURRENT", current)
     if (current === -1){
-        current = imageDeckLength -1 //because arrays are cardinal, I take the last number off the count to account for index 0
+        current = (imageDeckLength -1) //because arrays are cardinal, I take the last number off the count to account for index 0
+        console.log("CHANGED CURRENT FOR -1", current)
     };
 
     box[0].src=imagesDeck[current].name
     caption[0].innerHTML=imagesDeck[current].value
-    current -= 1
 });
 
 target[1].addEventListener("click", function(e) {
-    if (current > imageDeckLength){
+    current += 1
+    console.log("ADDED CURRENT", current)
+    if (current >= imageDeckLength){
         current = 0 //If i've gone through entire deck, start at begining
+        console.log("CHANGED CURRENT FOR len", current)
     }
     box[0].src=imagesDeck[current].name
     caption[0].innerHTML=imagesDeck[current].value
-    current += 1
 });
 
 
